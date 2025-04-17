36c783ea597dc033a96355d65b6566ef7d2499e2
mohammedsafik <mohammedsafik.m2022@sece.ac.in>
2025-04-17 12:43:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const promClient = require('prom-client');
+const app = express();
+const port = 5000;
+
+// Metrics setup
+const counter = new promClient.Counter({
+  name: 'webapp_requests_total',
+  help: 'Total number of requests to the web app'
+});
+
+promClient.collectDefaultMetrics();
+
+// Root route
+app.get('/', (req, res) => {
+  counter.inc();
+  res.send(`<h1 style="color:teal;">Hello DevOps World 🚀</h1>`);
+});
+
+// Prometheus metrics route
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
